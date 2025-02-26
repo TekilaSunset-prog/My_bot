98643b3f6428ade09666956f0186358962a73071
Academy <nam1koto01@gmail.com>
2025-02-26 11:09:40 +0500

    обнова

--- Bot/kinopoisk/functions.py
+++ Bot/kinopoisk/functions.py
@@ -44,7 +44,7 @@ def movie_href(movie_name):
                                 break
                             if flag is True:
                                 href += html[index1]
-    if category == 'series':
+    if category == 'series_':
         href = href.replace('film', 'series')
 
     return href + '/?utm_referrer=www.kinopoisk.ru', category
@@ -140,12 +140,19 @@ def movie_info(url, category):
             if not producers:
                 producers = 'Не найдены\n'
 
+            sp = soup.find('ul', class_='styles_list___ufg4').text
             actors = ''
-            sp = soup.find_all('li', class_='styles_root__vKDSE styles_rootInLight__EFZzH')
-            for actor in sp:
-                actors += '- ' + actor.text + '\n'
-            if not actors:
-                actors = 'Не найдены\n'
+            i = 0
+            actor = '- '
+
+            for symbol in sp:
+                if symbol.isupper():
+                    i += 1
+                if i == 3:
+                    actors += actor + '\n'
+                    actor = '- '
+                    i = 1
+                actor += symbol
 
         time = data.get('timeRequired')
         time1 = time
@@ -208,3 +215,59 @@ def movie_info(url, category):
             f'Возраст: {ages.text}\n\n'
             f'{family}'),
             f'{name}.png')
+
+    if category == 'series_':
+        headers = {
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cookie': 'yashr=1308056711739162196; _ym_uid=1739162199632805221; mobile=no; mda_exp_enabled=1; yandex_login=; i=rt9l6cN5yd+T4MRs7AKZFyXJ0a4AzUQg9X9sdl48lCA3Vw81QUG1VGqMoJ6QLKiM99AaXqjewEDqcYj9igFy4Pt8wLw=; yandexuid=2346427211709352542; my_perpages=%5B%5D; yuidss=2346427211709352542; _csrf=2e3PkZN55J-oOf70lsKErZx8; desktop_session_key=8bc2b861608a53b9ef3479c4e1afaf4017b9d83fbe24db20bb88df5744d5b12940453997e90c22341cbc7c44f890b9a974366a702d0d47f6e5053c21b3d6f01b23fa70cbe856f336c18b1c800cab09db07529906cf5c737f8f993eff5be9e395d7e80a9646a2b1322aa5f79c7748aa2f15ea05d36be931b04772f869b243c9107cdf107d81126eec3a32531ce4b9a71051b690344869ac5257c3db0f7f6229b338c516e06be75a57caca9e680f3d2a6b; desktop_session_key.sig=_hxcPFyGCyi1EHXMxxC9t_q5wdc; gdpr=0; _ym_isad=2; PHPSESSID=0639d71913d5d73405a98c2fd8ee885b; _csrf_csrf_token=e5k7j0tFCXYeJYFwBKm7GN6M0qRBiBEp6LFDvKWhqyQ; no-re-reg-required=1; _yasc=c5c8ZRC/dT2O8rvZBWo5Bo+2YYh67xDybwdOxTn8VlsQ9ho3IkAEwjTTFJfIuxEkurQ=; yp=1739341048.yu.2346427211709352542; ymex=1741846648.oyu.2346427211709352542; ya_sess_id=noauth:1739255263; sessar=1.1198.CiCMLxtvgmo-LjYtZ4jk5iiHIMuG2MBVWmlI8mEg58jmRw.wYEfb5OHeKfzI1SfCpnge6XnxbXGl_Tex6JTnRRPO8U; ys=c_chck.578514963; mda2_beacon=1739255263606; sso_status=sso.passport.yandex.ru:synchronized; initialUtm={%22utm_referrer%22:%22www.kinopoisk.ru%22}; finalUtm={%22utm_referrer%22:%22www.kinopoisk.ru%22}; cycada=/lGBltuKl4Aa6BxT6IuZYhGDEi26wU4MUjHeDo4cbWQ=; _ym_d=1739255300; _yasc=gOZZfnAAmMl2qpGkLfzaBgcvnTYlOetDcsdxvJdha5hzr+S8KEC+mj9h/rOkQTVAhbc=; _ym_d=1739262765; _ym_uid=1739162199632805221',
+            'priority': 'u=1, i',
+            'referer': 'https://www.kinopoisk.ru/film/198116/?utm_referrer=www.kinopoisk.ru',
+            'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'uber-trace-id': '4bb04c8f64df7279:1bff43fa7be80b11:0:1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+            'x-kl-safekids-ajax-request': 'Ajax_Request',
+            'x-request-id': '1739255300393287-555800689866559812:1',
+            'x-requested-with': 'XMLHttpRequest'
+        }
+        payload = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload, json=True)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        years = soup.find('span', class_="styles_brackets__zRUuj")
+        if years is None:
+            years = ''
+
+        info = soup.find_all('div', class_="styles_valueDark__BCk93 styles_value__g6yP4")
+        q = 0
+        genres = ''
+        directors = ''
+        producers = ''
+        time = ''
+
+        for i in info:
+            q += 1
+            if q == 2:
+                genres = i.text
+            if i.previous.text == 'Режиссер':
+                directors = i.text
+            if i.previous.text == 'Продюсер':
+                producers = i.text
+            if i.previous.text == 'Время серии':
+                time = i.text
+
+        if not genres:
+            genres = 'Не найдены'
+        if not directors:
+            directors = 'Не найдены'
+        if not producers:
+            producers = 'Не найдены'
+        if not time:
+            time = 'Неизвестно'
